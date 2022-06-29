90014186da22da280b29a633dee94a70e84d0d29
ilusha <ilya.yakov.com@gmail.com>
2022-06-29 12:28:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ input.addEventListener(
             const li = document.createElement('li');
             const content = `<img src='${country.flags.svg}' height="20" width="30" style='margin-right:10px'/>`;
             li.textContent = `${country.name.official}`;
-            li.style.listStyle = "none"
+            li.style.listStyle = 'none';
             list.append(li);
             li.insertAdjacentHTML('afterbegin', content);
           }
@@ -30,30 +30,26 @@ input.addEventListener(
             'Too many matches found. Please enter a more specific name.'
           );
         } else if (countries.length == 1) {
-          const headerDiv = document.createElement('div');
-          const capitalDiv = document.createElement('div');
-          const populationDiv = document.createElement('div');
-          const languagesDiv = document.createElement('div');
-          capitalDiv.textContent = `${countries[0].name.official}`;
-          headerDiv.textContent = `Capital: ${countries[0].capital}`;
-          populationDiv.textContent = `Population: ${countries[0].population}`;
-          const allLanguages = Object.values(countries[0].languages).join(', ');
-          languagesDiv.textContent = `Laguages: ${allLanguages}`;
-          const content = 
-          // `<svg height="70px" width="70px">
-          //         <use xlink:href="${countries[0].flags.svg} height="70" width="70"" height="70px" width="70px"></use>
-          //       </svg>`;
-          `<img src='${countries[0].flags.svg}' height="20" width="30" style='margin-right:10px'/>`;
-          headerDiv.insertAdjacentHTML('afterbegin', content);
-          info.append(headerDiv);
-          info.append(capitalDiv);
-          info.append(populationDiv);
-          info.append(languagesDiv);
+          correctCountry(countries[0])
         }
-      } catch {
-
-      }
-      
+      } catch {}
     });
   }, DEBOUNCE_DELAY)
 );
+function correctCountry(param){
+  const headerDiv = document.createElement('div');
+  const capitalDiv = document.createElement('div');
+  const populationDiv = document.createElement('div');
+  const languagesDiv = document.createElement('div');
+  capitalDiv.textContent = `${param.name.official}`;
+  headerDiv.textContent = `Capital: ${param.capital}`;
+  populationDiv.textContent = `Population: ${param.population}`;
+  const allLanguages = Object.values(param.languages).join(', ');
+  languagesDiv.textContent = `Laguages: ${allLanguages}`;
+  const content = `<img src='${param.flags.svg}' height="20" width="30" style='margin-right:10px'/>`;
+  headerDiv.insertAdjacentHTML('afterbegin', content);
+  info.append(headerDiv);
+  info.append(capitalDiv);
+  info.append(populationDiv);
+  info.append(languagesDiv);
+}
